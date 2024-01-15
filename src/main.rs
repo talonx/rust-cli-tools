a9ea76e542a5a2b9d8cdc4b9c1e0ed02c559b067
Hrishikesh Barua <talonx@gmail.com>
2024-01-15 16:43:09 +0530

    Ran rustfmt and added check for dir/file as target

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 use std::{fs, io};
 
 #[derive(Debug, Parser)]
 struct Cli {
     ///The command to run
     #[clap(subcommand)]
-    cmd: Command,       
-
+    cmd: Command,
 }
 
 #[derive(Debug, Subcommand)]
@@ -14,53 +14,56 @@ enum Command {
     ///list files and directories
     Ls {
         ///Show hidden files also
-        #[clap(short='a')]
+        #[clap(short = 'a')]
         a: bool,
         ///File or directory
         file: Option<String>,
     },
     ///Example command for testing
-    Ex {
-        b: String,
-    }
+    Ex { b: String },
 }
 
 fn main() {
-    
     let cli = Cli::parse();
     match &cli.cmd {
-        Command::Ls{a, file} => {
-//            println!("Listing {:?}", a);
+        Command::Ls { a, file } => {
+            //            println!("Listing {:?}", a);
             match ls(*a, file) {
-                Ok(()) => {},
+                Ok(()) => {}
                 Err(err) => {
                     println!("Error {}", err);
-                },
+                }
             }
         }
-        Command::Ex{b} => println!("Example command {}", b),
+        Command::Ex { b } => println!("Example command {}", b),
     }
     //println!("{:?}", cli);
 }
 
 //TODO support specific dir later
-fn ls(hidden: bool, target: &Option<String>) -> io::Result<()> {
-    let mut target_dir = ".";
-    match target {
-        Some(target) => target_dir = target,
-        None => {},
+fn ls(hidden: bool, file: &Option<String>) -> io::Result<()> {
+    let mut target = ".";
+    match file {
+        Some(file_or_dir) => target = file_or_dir,
+        None => {}
     }
-    for entry in fs::read_dir(target_dir)? {
-        let dir = entry?;
-        let name = dir.file_name().into_string();
-        match name {
-            Ok(p) => {
-                if hidden || !p.starts_with(".") {
-                    println!("{}", p);
+
+    if PathBuf::from(target).is_dir() {
+        for entry in fs::read_dir(target)? {
+            let dir = entry?;
+            let name = dir.file_name().into_string();
+            match name {
+                Ok(p) => {
+                    if hidden || !p.starts_with(".") {
+                        println!("{}", p);
+                    }
                 }
-            },
-            Err(_) => todo!(),
+                Err(_) => todo!(),
+            }
         }
+    } else {
+        //Until we have metadata support
+        println!("{}", target);
     }
     Ok(())
 }
